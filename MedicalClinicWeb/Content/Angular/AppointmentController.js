9227e3d6bd10747eafa406f0087a82d05f34d4d5
POWERRANGER\gis <fromeroy@gmail.com>
2018-04-15 18:16:42 -0600

    Updated AppointmentController.js

--- MedicalClinicWeb/Content/Angular/AppointmentController.js
+++ MedicalClinicWeb/Content/Angular/AppointmentController.js
@@ -1,6 +1,13 @@
 ﻿
 
-app.controller("appointmentController", function ($scope) {
-    $scope.FirstName = "Test";
-    $scope.LastName = "Test";
+app.controller("appointmentController", function ($routeParams, $scope, patientsService) {
+
+    var vm = this;
+
+    vm.patientId = $routeParams.patientId;
+
+    var patient = patientsService.getPatient(Patient.PatientID);
+
+    vm.patientName = patient.patientName;
+
 })
